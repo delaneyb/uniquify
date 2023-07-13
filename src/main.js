f5d47a249c9ae72ec5cf01092234603e48df20c5
Ben Delaney <ben@bdelaney.me>
2023-07-13 16:12:44 +1000

    Various improvements
    - Add removeTracksFromPlaylistOutsideBPMRange
    - Fix issue loading playlists in mounted
    - Fix x overflow, html width set 100%
    - Show BPM col conditional on if any tracks have it
    - Error handling
    - Auto open app only if port 9333 cdp working

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const SpotifyWebApi = require('spotify-web-api-node');  // https://github.com/th
 const { default: nodeFetch } = require('node-fetch')
 const path = require('path')
 const { execSync } = require('child_process')
-const PORT_NO = 3000;
+const PORT_NO = 30035;
 
 // NOTE: Getting Unauthorized errors means you need to get a new access token (todo: add ability for backend to refresh access token)
 
@@ -80,9 +80,13 @@ app.get('/spotifyAuth', (req, res) => {
  * Endpoint for retrieving Me object from spotify
  */
 app.get('/getMe', async (req, res) => {
-    const data = await spotifyApi.getMe()
-    console.log('Retrieved me object:', data.body);
-    res.send(data.body);
+    try {
+        const data = await spotifyApi.getMe()
+        console.log('Retrieved me object:', data.body);
+        res.send(data.body);
+    } catch (error) {
+        res.status(500).send(`Error: ${error.message || error}`);
+    }
 })
 
 /**
@@ -103,17 +107,22 @@ app.get('/getPlaylistTracks', async (req, res) => {
  * Not clear why this fails so often. Try getting playlists again then calling it
  */
 app.post('/getTracksFeatures', async (req, res) => {
-    let resp
-    try {
-        resp = await spotifyApi.getAudioFeaturesForTracks(req.body.trackIDs)
-        console.log(resp);
-        res.send(resp)
-    } catch (error) {
-        console.warn(error);
-        res.send(500, error)
+    let resp = []
+    const trackIds = req.body.trackIDs
+    const batch_size = 100
+    for (let i = 0; i < trackIds.length; i = i + batch_size) {
+        const current_batch = trackIds.slice(i, i + batch_size)
+        try {
+            const current_resp = await spotifyApi.getAudioFeaturesForTracks(current_batch).then(data => data.body.audio_features)
+            resp = [...resp, ...current_resp]
+        } catch (error) {
+            console.warn(error)
+            res.status(500).send({ error })
+            return
+        }
     }
-})
-
+    res.send(resp)
+});
 /**
  * Remove all tracks passed from the specified playlist for the specified user
  */
@@ -128,13 +137,13 @@ app.post('/deleteTracks', async (req, res) => {
             
             const resp = await spotifyApi.removeTracksFromPlaylist(req.query.userID, req.query.playlistID, tracksToRemove.slice(i, i + 100))
             console.log('deleteTracks got response from spotify:', resp);
-            res.send(resp)
         } catch (error) {
             console.warn('deleteTracks got error from spotify:', error);
             res.status(400)
             res.send({ error })
         }
     }
+    res.send({ removeCount: req.body.length })
 })
 
 /**
@@ -166,22 +175,25 @@ app.listen(PORT_NO, async () => {
     const url = `http://${(process.env.hostname || 'localhost')}:${PORT_NO}/`
     console.log(`Express server listening on ${url}`)
 
-    // Try activate existing app window or launch new Chrome app window to the page
-    const CHROME_CDP_PORT = 9333
-    const existingTab = await (await nodeFetch(`http://127.0.0.1:${CHROME_CDP_PORT}/json`)).json()
-        .then(tabs => tabs.find(t => t.url === url))
-        .catch(err => console.warn(`Unable to get Chrome tabs: ${err}`))
-    
-        if (existingTab) {
-        console.log(`Activating existing Chrome tab ${existingTab.id}`)
-        await nodeFetch(`http://127.0.0.1:${CHROME_CDP_PORT}/json/activate/${existingTab.id}`)
-    } else {
-        console.log(`No existing Chrome tab found, launching new Chrome app window`)
-        if (process.platform === 'win32') {
-            execSync(`start chrome -ArgumentList "--new-window --app=${url}"`, { shell: 'powershell' })
-        } else if (process.platform === 'darwin') {
-            execSync(`open -n -b com.google.Chrome --args --app=${url}`)
+    try {
+        // Try activate existing app window or launch new Chrome app window to the page
+        const CHROME_CDP_PORT = 9333
+        const existingTab = await (await nodeFetch(`http://127.0.0.1:${CHROME_CDP_PORT}/json`)).json()
+            .then(tabs => tabs.find(t => t.url === url))
+            .catch(err => console.warn(`Unable to get Chrome tabs: ${err}`))
+        
+            if (existingTab) {
+            console.log(`Activating existing Chrome tab ${existingTab.id}`)
+            await nodeFetch(`http://127.0.0.1:${CHROME_CDP_PORT}/json/activate/${existingTab.id}`)
+        } else {
+            console.log(`No existing Chrome tab found, launching new Chrome app window`)
+            if (process.platform === 'win32') {
+                execSync(`start chrome -ArgumentList "--new-window --app=${url}"`, { shell: 'powershell' })
+            } else if (process.platform === 'darwin') {
+                execSync(`open -n -b com.google.Chrome --args --app=${url}`)
+            }
         }
+    } catch (error) {
+        console.log(`Unable to launch Chrome app window: ${error}`)
     }
-
 })
--- src/static/vueApp.js
+++ src/static/vueApp.js
@@ -15,8 +15,8 @@ var vueApp = new Vue({
     data: {
         accessToken: lsCache.fetch('accessToken'),
         /** null if not logged in/undefined */
-        me: lsCache.fetch('me'),
-        playlists: lsCache.fetch('playlists'),
+        me: null,
+        playlists: [],
         playlistsFilter: '',
         /** playlist to remove songs from */
         removeFrom: undefined,
@@ -31,6 +31,17 @@ var vueApp = new Vue({
         playlists: {
             handler: lsCache.save.bind(null, 'playlists'),
             deep: true  // Also make sure to save when we e.g. fetch the tracks within a playlist
+        }
+    },
+    async mounted() {
+        try {
+            await this.getMe()
+            await this.getMyPlaylists()
+        } catch (error) {
+            if (/Unauthorized/i.test(error)) {
+                // Need to authenticate first
+                this.accessToken = ''
+            }
         }
     },
     computed: {
@@ -102,9 +113,17 @@ var vueApp = new Vue({
                 return tr.track.name.toLowerCase() == track.track.name.toLowerCase()
             })
         },
-        async getMyDetails() {
+        async getMe() {
             const resp = await fetch('/getMe', this.apiRequestOptions);
+            if (resp.status !== 200) throw new Error(await resp.text())
             this.me = await resp.json();
+        },
+        async getMyDetailsClicked() {
+            try {
+                await this.getMe()
+            } catch (error) {
+                alert(error)
+            }
         },
         async getMyPlaylists() {
             const resp = await fetch(`/getPlaylists?userID=${this.me.id}`, this.apiRequestOptions);
@@ -127,13 +146,13 @@ var vueApp = new Vue({
                 })
             });
             // Just update each track object, leaving it inside that particular playlist
-            const featuresByTrack = (await resp.json()).body.audio_features.forEach((v, i) => {
+            const featuresByTrack = (await resp.json()).forEach((v, i) => {
                 tracks[i].track = { ...tracks[i].track, ...v }
             })
             console.log('Track features:', featuresByTrack);
         },
-        async deleteTracks(playlist, tracksToRemove) {
-            const deleteURIs = this.removeFromTracks.filter(t => this.willBeDeleted(t)).map(tr => tr.track.uri)
+        async deleteTracks(playlist, deleteURIs) {
+            deleteURIs ||= this.removeFromTracks.filter(t => this.willBeDeleted(t)).map(tr => tr.track.uri)
             console.log('Deleting from playlist', playlist.id, 'tracks with uris:', deleteURIs);
             const resp = await fetch(`/deleteTracks?userID=${this.me.id}&playlistID=${playlist.id}`, {
                 method: "POST", // *GET, POST, PUT, DELETE, etc.
@@ -151,5 +170,27 @@ var vueApp = new Vue({
                 console.warn('Backend returned error while trying to delete tracks', respObj.error);
             }
         },
+        async removeTracksFromPlaylistOutsideBPMRange() {
+            debugger
+            // Get the features of all the tracks in the selected playlist if we dont already have them
+            if (!this.removeFromTracks.every(tr => tr.track.tempo)) await this.getTracksFeatures(this.removeFromTracks)
+            // Collect one or more low and high BPM input ranges via browser prompt comma separated.
+            // Example: 85-95,175-185 should result in 2 ranges of 85-95 and 175-185
+            const ranges = prompt('Enter one or more BPM ranges to filter tracks by, comma separated. Example: 85-95,175-185', '85-95,175-185')
+            // Split the ranges into an array
+            const rangesArray = ranges.split(',')
+            // Filter out any empty strings
+            const rangesArrayFiltered = rangesArray.filter(r => r)
+            // For each range, split it into min and max BPM
+            const rangesArrayFilteredSplit = rangesArrayFiltered.map(r => r.split('-'))
+            // For each range, convert the min and max BPM to numbers
+            const rangesArrayFilteredSplitNum = rangesArrayFilteredSplit.map(r => r.map(n => Number(n)))
+            // Find tracks to be removed - any that dont fall into at least one of the ranges
+            const tracksToRemove = this.removeFromTracks.filter(tr => !rangesArrayFilteredSplitNum.some(r => tr.track.tempo >= r[0] && tr.track.tempo <= r[1]))
+            // Confirm
+            if (!confirm(`Are you sure you want to remove ${tracksToRemove.length} tracks from ${this.removeFrom.name}?`)) return
+            // Delete the tracks from the playlist
+            await this.deleteTracks(this.removeFrom, tracksToRemove.map(tr => tr.track.uri))
+        }
     }
 })
